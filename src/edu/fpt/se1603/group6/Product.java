9c4a43989d5e3486ecbb29d722765b3067e69d83
Radkaim <hungct04@gmail.com>
2022-01-16 16:00:40 +0700

    Add class Product

--- src/edu/fpt/se1603/group6/Product.java
+++ src/edu/fpt/se1603/group6/Product.java
@@ -13,13 +13,6 @@ public class Product {
         quantity = 0;
     }
 
-    public Product(int id, String name, double price, int quantity) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-    }
-
     public int getId() {
         return id;
     }
