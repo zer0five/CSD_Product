28c82d0b5d7260b4521c7596d4e900e1ddb6a2f5
K4zoku <kazoku0356@gmail.com>
2022-01-16 15:24:34 +0700

    remove examples

--- src/edu/fpt/se1603/group6/Main.java
+++ src/edu/fpt/se1603/group6/Main.java
@@ -3,8 +3,5 @@ package edu.fpt.se1603.group6;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("Radkaim");
-        System.out.println("Khoa");
     }
 }
