5f1e4a579b3209b335da8d40452eaf7a42405d67
khoatdt <tuankhoa17402@gmail.com>
2022-01-16 17:10:05 +0700

    Added javadocs for ProductSerialize

--- src/edu/fpt/se1603/group6/ProductSerialize.java
+++ src/edu/fpt/se1603/group6/ProductSerialize.java
@@ -2,22 +2,52 @@ package edu.fpt.se1603.group6;
 
 import java.io.*;
 import java.util.LinkedList;
-import java.util.Objects;
 
+/**
+ * class ProductSerialize
+ *
+ * @author Trịnh Đức Tuấn Khoa - CE160799
+ */
 public class ProductSerialize {
+    /**
+     * File of file
+     */
     private File file;
+
+    /**
+     *Constructor of productSerialize
+     *
+     * @param fileName
+     */
     public ProductSerialize(String fileName) {
         this.file = new File(fileName);
     }
 
+    /**
+     * Get file of productSerialize
+     *
+     * @author Trịnh Đức Tuấn Khoa - CE160799
+     * @return file
+     */
     public File getFile() {
         return file;
     }
 
+    /**
+     * Set file of productSerialize
+     *
+     * @author Trịnh Đức Tuấn Khoa - CE160799
+     */
     public void setFile(File file) {
         this.file = file;
     }
 
+    /**
+     * Load Products to LinkedList
+     *
+     * @author Trịnh Đức Tuấn Khoa - CE160799
+     * @return The list
+     */
     public LinkedList<Product> loadProducts() {
         LinkedList<Product> list = new LinkedList<>();
         if (file.exists()) {
@@ -33,6 +63,12 @@ public class ProductSerialize {
         }
     }
 
+    /**
+     * Save Products to file
+     *
+     * @author Trịnh Đức Tuấn Khoa - CE160799
+     * @return true if success, otherwise false
+     */
     public boolean saveProducts(LinkedList<Product> list) {
         try {
             FileOutputStream fos = new FileOutputStream(file);
