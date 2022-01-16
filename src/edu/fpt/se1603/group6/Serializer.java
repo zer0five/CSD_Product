87915a662b1667ea0b7f2652a9add7fd077a2926
K4zoku <kazoku0356@gmail.com>
2022-01-16 18:27:12 +0700

    rearranged javadocs for Serializer interface

--- src/edu/fpt/se1603/group6/Serializer.java
+++ src/edu/fpt/se1603/group6/Serializer.java
@@ -3,25 +3,25 @@ package edu.fpt.se1603.group6;
 /**
  * interface Serializer
  *
- * @param <T> the type of the object to be serialized
  * @author Giang Trương Gia Lạc
+ * @param <T> the type of the object to be serialized
  */
 public interface Serializer<T> {
 
     /**
      * Serialize the object to the file
      *
+     * @author Giang Trương Gia Lạc
      * @param t the object to be serialized
      * @return true if the serialization is successful
-     * @author Giang Trương Gia Lạc
      */
     boolean serialize(T t);
 
     /**
      * Deserialize the object from the file
      *
-     * @return the object after deserialization
      * @author Giang Trương Gia Lạc
+     * @return the object after deserialization
      */
     T deserialize();
 
