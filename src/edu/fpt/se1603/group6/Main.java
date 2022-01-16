d5f81009abef18ffaf362bb51bd8fc72f89db7fd
khoatdt <tuankhoa17402@gmail.com>
2022-01-16 14:55:18 +0700

    khoa

--- src/edu/fpt/se1603/group6/Main.java
+++ src/edu/fpt/se1603/group6/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello");
+<<<<<<< HEAD
         System.out.println("Radkaim");
 	    
+=======
+        System.out.println("Khoa");
+>>>>>>> fc31040 (khoa)
     }
 }
