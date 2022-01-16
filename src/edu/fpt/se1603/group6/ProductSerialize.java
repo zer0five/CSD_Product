098bc4efacdeab29f44561116a1237988cd7de91
khoatdt <tuankhoa17402@gmail.com>
2022-01-16 16:43:42 +0700

    Added ProductSerialize

---
+++ src/edu/fpt/se1603/group6/ProductSerialize.java
@@ -0,0 +1,35 @@
+package edu.fpt.se1603.group6;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Objects;
+
+public class ProductSerialize {
+    private File file;
+    public ProductSerialize(String fileName) {
+        this.file = new File(fileName);
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public LinkedList<Product> loadProducts() {
+        LinkedList<Product> list = new LinkedList<>();
+        if (file.exists()) {
+            try {
+                FileInputStream fis = new FileInputStream(file);
+                ObjectInputStream ois = new ObjectInputStream(fis);
+                return (LinkedList<Product>) ois.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                return list;
+            }
+        } else {
+           return list;
+        }
+    }
+}
