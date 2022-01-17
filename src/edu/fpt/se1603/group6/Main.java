bdbd46b77da8f94c0011e254c671ae549a547822
khoatdt <tuankhoa17402@gmail.com>
2022-01-17 08:33:03 +0700

    Test conflict

--- src/edu/fpt/se1603/group6/Main.java
+++ src/edu/fpt/se1603/group6/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("+-----+------------------+--------+----+------+");
         System.out.println("|  ID |    Name          |");
-
+        System.out.println("Happy New Year");
     }
 }
