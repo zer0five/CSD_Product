4b308841eceec2424faf18321d66a0663f8a7a76
K4zoku <kazoku0356@gmail.com>
2022-01-17 10:07:36 +0700

    update javadocs for Manager

--- src/edu/fpt/se1603/group6/Manager.java
+++ src/edu/fpt/se1603/group6/Manager.java
@@ -2,23 +2,73 @@ package edu.fpt.se1603.group6;
 
 import java.util.LinkedList;
 
+/**
+ * Abstract class for Manager
+ *
+ * @param <T> type of object that Manager manages
+ * @author Giang Trương Gia Lạc
+ */
 public abstract class Manager<T> {
 
+    /**
+     * List of managed objects
+     */
     protected LinkedList<T> list;
 
+    /**
+     * Constructor for Manager
+     *
+     * @param list list of managed objects
+     * @author Giang Trương Gia Lạc
+     */
     protected Manager(LinkedList<T> list) {
         this.list = list;
     }
 
+    /**
+     * Get user choice
+     *
+     * @return user choice
+     * @author Giang Trương Gia Lạc
+     */
     public abstract String choice();
 
+    /**
+     * Handle user choice
+     *
+     * @param choice user choice
+     * @return true if continued, false if stop
+     * @author Giang Trương Gia Lạc
+     */
     public abstract boolean handle(String choice);
 
+    /**
+     * Input a new object
+     *
+     * @return new object
+     * @author Giang Trương Gia Lạc
+     */
     public abstract T input();
 
+    /**
+     * Add a new object from user input
+     *
+     * @author Giang Trương Gia Lạc
+     */
     public abstract void add();
 
+    /**
+     * Update an object information
+     *
+     * @author Giang Trương Gia Lạc
+     */
     public abstract void update();
 
+    /**
+     * Display all objects
+     *
+     * @author Giang Trương Gia Lạc
+     */
     public abstract void display();
+
 }
