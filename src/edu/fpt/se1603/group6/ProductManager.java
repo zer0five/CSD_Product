28a7a49a0a5785010fd9bd162c4b76a1f9ddc593
K4zoku <kazoku0356@gmail.com>
2022-01-17 11:29:32 +0700

    Update ProductManager

--- src/edu/fpt/se1603/group6/ProductManager.java
+++ src/edu/fpt/se1603/group6/ProductManager.java
@@ -83,7 +83,7 @@ public class ProductManager extends Manager<Product> {
     public Product input() {
         Product product = new Product();
         while (true) {
-            int id = input.getInt("Please Enter product id: ");
+            int id = input.getInt("Please enter product id: ");
             Optional<Product> productOptional = getProductById(id);
             if (productOptional.isPresent()) {
                 System.out.println("Id is existed, please try again!");
@@ -92,9 +92,9 @@ public class ProductManager extends Manager<Product> {
                 break;
             }
         }
-        product.setName(input.getStringNotEmpty("Please Enter product name: "));
-        product.setQuantity(input.getInt("Please Enter product quantity: ", 0));
-        product.setPrice(input.getDouble("Please Enter product price: ", 0));
+        product.setName(input.getStringNotEmpty("Please enter product name: "));
+        product.setQuantity(input.getInt("Please enter product quantity: ", 0));
+        product.setPrice(input.getDouble("Please enter product price: ", 0));
         return product;
     }
 
@@ -115,7 +115,7 @@ public class ProductManager extends Manager<Product> {
      */
     public void search() {
         System.out.println("====[ Find product ]====");
-        String query = input.getString("Please Enter product name: ").toLowerCase();
+        String query = input.getString("Please enter product name: ").toLowerCase();
         if (query.isEmpty()) {
             display();
         } else {
@@ -163,7 +163,7 @@ public class ProductManager extends Manager<Product> {
         System.out.println("====[ Edit product ]====");
         Product product;
         while (true) {
-            int id = input.getInt("Please Enter product id: ");
+            int id = input.getInt("Please enter product id: ");
             Optional<Product> optionalProduct = getProductById(id);
             if (optionalProduct.isPresent()) {
                 product = optionalProduct.get();
@@ -172,6 +172,11 @@ public class ProductManager extends Manager<Product> {
                 System.out.println("Product with id " + id + " is not existed!");
             }
         }
+        System.out.println("+-----+--------------------+----------+-------+--------+");
+        System.out.println("| ID  | Product            | Quantity | Price | Amount |");
+        System.out.println("+-----+--------------------+----------+-------+--------+");
+        System.out.println(product);
+        System.out.println("+-----+--------------------+----------+-------+--------+");
         double price = input.getDouble("Please input new price for product: ", 0);
         if (price == product.getPrice()) {
             System.out.println("Warning: Price is not changed!");
