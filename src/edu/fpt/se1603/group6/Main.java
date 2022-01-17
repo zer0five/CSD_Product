1545e9a426e1eb65239abdb60274f70314bf3fa9
K4zoku <kazoku0356@gmail.com>
2022-01-17 08:36:45 +0700

    clean up

--- src/edu/fpt/se1603/group6/Main.java
+++ src/edu/fpt/se1603/group6/Main.java
@@ -4,9 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("+-----+------------------+--------+----+------+");
-        System.out.println("|  ID |    Name          |");
-        System.out.println("Happy New Year");
-
-        System.out.println("hùng");
     }
 }
