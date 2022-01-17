795eff2fe61015afbd25508d98ade2eb4ae8c0a7
K4zoku <kazoku0356@gmail.com>
2022-01-17 10:31:26 +0700

    added javadocs for Main

--- src/edu/fpt/se1603/group6/Main.java
+++ src/edu/fpt/se1603/group6/Main.java
@@ -2,8 +2,18 @@ package edu.fpt.se1603.group6;
 
 import java.util.LinkedList;
 
+/**
+ * Main class
+ *
+ * @author Group 6 members
+ */
 public class Main {
 
+    /**
+     * Main method
+     *
+     * @author Group 6 members
+     */
     public static void main(String[] args) {
         ProductsSerializer productsSerializer = new ProductsSerializer("products.txt");
         LinkedList<Product> products = productsSerializer.deserialize();
@@ -14,4 +24,5 @@ public class Main {
         } while (productManager.handle(choice));
         productsSerializer.serialize(productManager.getProducts());
     }
+
 }
